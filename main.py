f29924905cb556c1bc797f29c66deb3ec798ec32
Stanislavpetkov17 <stanislavpetkov34@gmail.com>
2026-08-13 19:34:45 +0300

    add functionality with json file

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import json
+
+#open json
+with open("tasks.json", "r") as file:
+    tasks = json.load(file)
+
+#add new task
 task = {}
 description = input("Add task: ")
 task["description"] = description
-tasks = json.dumps(task)
+tasks.append(task)
+
+#write to json
+with open("tasks.json", "w") as file:
+    json.dump(tasks, file)
+
 print(tasks)
 
 
