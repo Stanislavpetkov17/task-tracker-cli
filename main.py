28e3a16ea6115802a1d58f1ed34b92cbe423282a
Stanislavpetkov17 <stanislavpetkov34@gmail.com>
2026-08-13 23:15:44 +0300

    change ID algorithm to prepare for task deletion

--- main.py
+++ main.py
@@ -18,10 +18,16 @@ if command == "Add" or command == "add":
     task = {}
     description = input("Add task: ")  #add the
     task["description"] = description  #description
-    tasks.append(task)                 #append new task to list
-    id = task["id"] = len(tasks)       #add ID
 
+    #new id algo
+    highest_id = 0
+    for existing_task in tasks:
+        if existing_task["id"] > highest_id:
+            highest_id = existing_task["id"]
+    new_id = highest_id + 1
 
+    task["id"] = new_id            #add ID
+    tasks.append(task)             #append new task to list
 
 #write to json
 with open("tasks.json", "w") as file:
