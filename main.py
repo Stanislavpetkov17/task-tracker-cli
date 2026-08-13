df795cff1aeda5c6737126a1c92df7524a05d77d
Stanislavpetkov17 <stanislavpetkov34@gmail.com>
2026-08-13 20:58:46 +0300

    add command interface and task clearing

--- main.py
+++ main.py
@@ -4,11 +4,24 @@ import json
 with open("tasks.json", "r") as file:
     tasks = json.load(file)
 
+command = input("Add, Update, Delete or Clear: ")
+
+#clear the whole thing
+if command == "Clear" or command == "clear":
+    tasks = []
+
+
+
+
 #add new task
-task = {}
-description = input("Add task: ")
-task["description"] = description
-tasks.append(task)
+if command == "Add" or command == "add":
+    task = {}
+    description = input("Add task: ")  #add the
+    task["description"] = description  #description
+    tasks.append(task)                 #append new task to list
+    id = task["id"] = len(tasks)       #add ID
+
+
 
 #write to json
 with open("tasks.json", "w") as file:
