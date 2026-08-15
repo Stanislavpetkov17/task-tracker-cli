b6d4b76ddcbe2a016656bce0160cd96f2bc42ddb
Stanislavpetkov17 <stanislavpetkov34@gmail.com>
2026-08-15 19:56:53 +0300

    add update functionality

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ with open("tasks.json", "r") as file:
 while True:
     command = input("\n1:Add\n2:Update\n3:Delete\n4:Clear\n5:List\n6:Exit\nChoose an action: ")
 
-    #clear the whole thing
-    if command == "4":
-        tasks = []
-
     #add new task
     if command == "1":
         task = {}
@@ -24,16 +20,34 @@ while True:
         new_id = highest_id + 1
 
         task["id"] = new_id            #add ID
+        task["status"] = "To-Do"       #add status
         tasks.append(task)             #append new task to list
 
-    #write to json
-    with open("tasks.json", "w") as file:
-        json.dump(tasks, file)
+    #update task
+    if command == "2":
+        id_to_update = input("Enter task ID: ")
+        id_to_update = int(id_to_update) 
+        #find task with inputted id
+        for task_to_update in tasks:
+            if task_to_update["id"] == id_to_update:
+                #update status
+                new_status = input("1:To-Do\n2:In-Progress\n3:Completed\nChoose new status: ")
+                if new_status == "1":
+                    task_to_update["status"] = "To-Do"
+                if new_status == "2":
+                    task_to_update["status"] = "In-Progress"
+                if new_status == "3":
+                    task_to_update["status"] = "Completed"
 
-    if command == "5":
+    if command == "4": #clear
+        tasks = []
+
+    if command == "5": #list
         print()
         print(tasks)
-    if command == "6":
+    if command == "6": #exit
         break
 
-
+    #save to json
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file)
